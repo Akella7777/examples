ad20f791f3eb4d3b31f8c9382db0d7cca18e5807
Mikhail Rudikov <hrom7777@mail.ru>
2023-02-08 23:45:56 +0500

    add 7 examples

---
+++ example002/Program.cs
@@ -0,0 +1,7 @@
+﻿//Console.WriteLine("Введите Ваше имя ");
+//string username = Console.ReadLine();
+//Console.WriteLine("Привет, " + username);
+
+Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, " + username); 
---
+++ example003/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = 3;
+int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+int numberC = new Random().Next(1,10); // 1 2 3 .. 9
+Console.WriteLine(numberC);
+int numberD = new Random().Next(1,10); // 1 2 3 .. 9
+Console.WriteLine(numberD);
+//Console.WriteLine(numberA + numberB);
+int result2 = numberC + numberD;
+Console.WriteLine(result2); 
